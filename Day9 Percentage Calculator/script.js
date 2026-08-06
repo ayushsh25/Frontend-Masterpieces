36066c2564f9f2d22ca9bbbe7bc88f4f0591cffb
Aayush Shah <163997723+ayushsh25@users.noreply.github.com>
2026-08-06 18:27:39 +0530

    Add files via upload
    A responsive Percentage Calculator built with HTML, CSS, and JavaScript that performs fast and accurate percentage calculations through a clean and user-friendly interface.

---
+++ Day9 Percentage Calculator/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const percentageSlider = document.getElementById("percentageSlider");
+    const percentageValue = document.getElementById("percentageValue");
+
+    percentageValue.textContent = `${percentageSlider.value}%`;
+
+    percentageSlider.addEventListener("input", function () {
+        percentageValue.textContent = `${percentageSlider.value}%`;
+    });
+
+});
+
+function calculatePercentage() {
+
+    const percentageSlider = document.getElementById("percentageSlider").value;
+    const baseNumber = document.getElementById("baseNumber").value;
+
+    if (baseNumber === "") {
+        alert("Please enter the base number");
+        return;
+    }
+
+    const result = (
+        parseFloat(baseNumber) * parseFloat(percentageSlider) / 100
+    ).toFixed(2);
+
+    document.getElementById("result").innerHTML = `Result: ${result}`;
+}
